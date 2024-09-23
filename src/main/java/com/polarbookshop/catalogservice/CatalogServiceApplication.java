ac73495f73b563c65b34f935f97dd8ea980e2b52
DS0708 <jonum12312@gmail.com>
2024-09-23 17:33:22 +0900

    4.1 스프링 설정 : 속성과 프로파일

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan    //@ConfigurationProperties 어노테이션이 붙어 있는 클래스들을 스캔하여 스프링의 application context에 빈으로 등록
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
